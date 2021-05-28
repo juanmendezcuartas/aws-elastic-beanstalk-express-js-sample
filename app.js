a7c879bda2f94ec5ffaad793bfbeeaf60520540f
Juan Mendez <juanmendezcuartas@gmail.com>
2021-05-27 20:44:13 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Soy un DevOps"));
+app.get("/", (req, res) =>
+  res.send("Soy un DevOps y futuro Senior DevOps en AWS")
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
